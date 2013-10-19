d53e05b43c55adf5f26aa29b23bb1ae4b9699191
Paul Vollmer <paul.vollmer@fh-potsdam.de>
2013-10-20 00:21:19 +0200

    Changed module.exports to exports.readFile, update test and cli

--- test/test.lib.index.js
+++ test/test.lib.index.js
@@ -4,10 +4,12 @@ var rci = require('../lib');
 
 describe('lib/index.js', function() {
 
-  it('test with no-include.txt', function() {
-    rci('./test/files/no-include.txt', '<%>', '</%>', function(data) {
-      var expected = 'no include at this file\n';
-      assert.equal(expected, data);
+  describe('#readFile()', function() {
+    it('test with no-include.txt', function() {
+      rci.readFile('./test/files/no-include.txt', '<%>', '</%>', function(data) {
+        var expected = 'no include at this file\n';
+        assert.equal(expected, data);
+      });
     });
   });
 
