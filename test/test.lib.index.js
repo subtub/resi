1c54dc52e17e65fbed9193dfc0d6426d6b7c5f28
Paul Vollmer <paul.vollmer@fh-potsdam.de>
2013-10-20 00:20:00 +0200

    Added isUrl function and test

--- test/test.lib.index.js
+++ test/test.lib.index.js
@@ -11,4 +11,16 @@ describe('lib/index.js', function() {
     });
   });
 
+
+  describe('#isUrl()', function() {
+    it('should return true if it is a http url.', function() {
+      assert.equal(true, rci.isUrl('http://www.subtub.io/'));
+    });
+    it('should return true if it is a https url.', function() {
+      assert.equal(true, rci.isUrl('https://www.subtub.io/'));
+    });
+    it('should return false if it is not a url.', function() {
+      assert.equal(false, rci.isUrl('foo/bar'));
+    });
+  });
 });
