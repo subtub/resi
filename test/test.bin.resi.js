d9c421ea2718608a9c6e5e7a10b1691b3ce4a3e7
Paul Vollmer <paul.vollmer@fh-potsdam.de>
2013-10-25 08:45:18 +0200

    Url changes after github repo renaming

--- test/test.bin.resi.js
+++ test/test.bin.resi.js
@@ -6,8 +6,8 @@ describe('bin/resi', function() {
   testExec('');
   testExec('-i test/files/no-include.txt');
   testExec('-i test/files/include-file.txt -o test/files/_test_1.txt');
-  testExec('-i https://raw.github.com/subtub/RecursiveContentInclude/master/test/files/include-file.txt');
-  testExec('-i https://raw.github.com/subtub/RecursiveContentInclude/master/test/files/include-file.txt -o test/files/_test_2.txt');
+  testExec('-i https://raw.github.com/subtub/resi/master/test/files/include-file.txt');
+  testExec('-i https://raw.github.com/subtub/resi/master/test/files/include-file.txt -o test/files/_test_2.txt');
 });
 
 /**
