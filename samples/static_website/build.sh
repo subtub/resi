9bc34219898be590d0449f43b62bf281f6bb8ba9
Paul Vollmer <paul.vollmer@fh-potsdam.de>
2013-11-14 12:47:31 +0100

    Created static website sample

---
+++ samples/static_website/build.sh
@@ -0,0 +1,5 @@
+mkdir generated_files
+
+node ../../bin/resi -i content/page1.html -o generated_files/page1.html
+node ../../bin/resi -i content/page2.html -o generated_files/page2.html
+node ../../bin/resi -i content/page3.html -o generated_files/page3.html
